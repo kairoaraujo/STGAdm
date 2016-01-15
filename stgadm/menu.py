2ab89cd9ba893efcc4bd46fb4d15c8bd884061bf
kairoaraujo <kairo@kairo.eti.br>
2016-01-15 18:10:32 -0200

    menu restructured

--- stgadm/menu.py
+++ stgadm/menu.py
@@ -11,7 +11,7 @@ import emc_cmds
 
 
 def main_menu():
-    global chk_server, disk_volume, lun_size, pool_option
+    global disk_volume, lun_size
 
     os.system('clear')
     print('[ Storage Adm ]\n[ Version {0} - © 2015 ]\n\n'.format(
@@ -68,94 +68,152 @@ def main_menu():
         get_stg = systemstorages.SystemStorages()
         get_stg.selectstorage()
 
-        # get storage informations
-        chk_server = emc_cmds.VMAX(config.symcli_path, get_stg.getsid(),
-                                   wwn_client)
+        stg_type = get_stg.gettype()
 
-        print('\nCollecting some storage informations. Please wait...')
+        if stg_type == 'EMC_VMAX':
+            stg_sid = get_stg.getsid()
+            stg_name = get_stg.getstorage()
+            menu_emc_vmax(stg_name, stg_type, stg_sid, wwn_client,
+                          hostname_client, storage_name)
 
-        ign = chk_server.get_ign(wwn_client)
+        elif stg_type == 'EMC_VNX':
+            pass
 
-        if ign[0] != 0:
-            print('ERROR: {0}'.format(ign[1].replace('\n', '')))
-            exit(1)
         else:
-            ign = ign[1]
+            print('ERRO: Storage type {0} invalid. Check config file'.format(
+                stg_type))
 
-        mvn = chk_server.get_mvn(ign)
 
-        if mvn[0] != 0:
-            print('ERROR: {0}'.format(mvn[1]))
-            exit(1)
+def menu_emc_vmax(stg_name, stg_type, stg_sid, wwn_client, change=None,
+                  hostname_client=None, storage_name=None):
+    # get storage informations
+
+    global pool_option
+
+    chk_server = emc_cmds.VMAX(config.symcli_path, stg_sid,
+                               wwn_client)
+
+    print('\nCollecting some storage informations. Please wait...')
+
+    ign = chk_server.get_ign(wwn_client)
+
+    if ign[0] != 0:
+        print('ERROR: {0}'.format(ign[1].replace('\n', '')))
+        exit(1)
+    else:
+        ign = ign[1]
+
+    mvn = chk_server.get_mvn(ign)
+
+    if mvn[0] != 0:
+        print('ERROR: {0}'.format(mvn[1]))
+        exit(1)
+    else:
+        mvn = mvn[1]
+
+    sgn = chk_server.get_sgn(mvn)
+
+    if sgn[0] != 0:
+        print('ERROR: {0}'.format(sgn[1]))
+        exit(1)
+    else:
+        sgn = sgn[1]
+
+    print('\nGetting information about pools from storage. Please wait...')
+
+    # get storage pools
+    lspool = chk_server.lspools()
+
+    # check if command worked well.
+    if lspool[0] != 0:
+        print('Error: {0}'.format(lspool[1]))
+        exit(1)
+
+    lspool = lspool[1]
+    print lspool
+    lspool = lspool.split('\n')
+
+    print('Select the Storage Pool from {0} storage (SID:{1})\n'
+          .format(stg_name, stg_sid))
+
+    # creating array to select storage pool
+    pool_list = []
+
+    for l_lspool in lspool[8:]:
+        if l_lspool == '':
+            break
         else:
-            mvn = mvn[1]
+            pool_list.append(l_lspool.split()[0])
 
-        sgn = chk_server.get_sgn(mvn)
+    count = 0
+    for l_pool in pool_list:
+        print ('{0}: {1}'.format(count, l_pool))
+        count += 1
 
-        if sgn[0] != 0:
-            print('ERROR: {0}'.format(sgn[1]))
-            exit(1)
+    while True:
+        try:
+            pool_option = int(raw_input("Select the pool: "))
+            break
+        except IndexError:
+            print(
+                '\tERROR: Select an existing option between'
+                '0 and {0}.'.format(count))
+
+    print('\nConfig validation\n')
+    print('\nClient Information')
+    print(50 * '-')
+    print('Register      : {0}'.format(change))
+    print('Client Server : {0}'.format(hostname_client))
+    print('Storage Name  : {0}'.format(storage_name))
+    print('WWN Client    : {0}'.format(wwn_client))
+    print('\nStorage Information')
+    print(50 * '-')
+    print('Storage Name          : {0}'.format(stg_name))
+    print('Storage Type          : {0}'.format(stg_type))
+    print('Storage SID           : {0}'.format(stg_sid))
+    print('Initiator Group Name  : {0}'.format(ign))
+    print('Making View Names     : {0}'.format(mvn))
+    print('Storage Group Name    : {0}'.format(sgn))
+    print('Storage Pool          : {0}'.format(pool_list[pool_option]))
+    print('\nInformations about the request:')
+    print(50 * '-')
+    print('Disk Volume   : {0}'.format(disk_volume))
+    print('LUN Size      : {0}'.format(lun_size))
+    print('')
+
+    def choose_device_type():
+        print('Select the type of device you want create:\n')
+        print('0. Meta device')
+        print('1. Regular device')
+        device_type = raw_input('\nChoose an option: ')
+
+        if device_type == '0':
+
+            while True:
+                try:
+                    size_check = 1
+
+                    while size_check != 0:
+                        member_size = int(raw_input(
+                            'What is the size of member? (GB): '))
+                        size_check = lun_size % member_size
+                        if size_check != 0:
+                            print('ERRO: The member size ({0} GB) needs to '
+                                  'be an integer divisor by the '
+                                  'LUN Size ({1}GB).'
+                                  .format(member_size, lun_size))
+
+                    break
+                except (TypeError, ValueError):
+                    print(
+                        '\tERROR: Total Disk need to be an int value in GB.'
+                        '\nDo not use GB. Example: 1000 for 1000GB (1TB)')
+
+        elif device_type == '1':
+            pass
+
         else:
-            sgn = sgn[1]
+            print('ERRO: Wrong option.')
+            choose_device_type()
 
-        print('\nGetting information about pools from storage. Please wait...')
-
-        # get storage pools
-        lspool = chk_server.lspools()
-
-        # check if command worked well.
-        if lspool[0] != 0:
-            print('Error: {0}'.format(lspool[1]))
-            exit(1)
-
-        lspool = lspool[1]
-        print lspool
-        lspool = lspool.split('\n')
-
-        print('Select the Storage Pool from {0} storage (SID:{1})\n'
-              .format(get_stg.getstorage(), get_stg.getsid()))
-
-        # creating array to select storage pool
-        pool_list = []
-
-        for l_lspool in lspool[8:]:
-            if l_lspool == '':
-                break
-            else:
-                pool_list.append(l_lspool.split()[0])
-
-        count = 0
-        for l_pool in pool_list:
-            print ('{0}: {1}'.format(count, l_pool))
-            count += 1
-
-        while True:
-            try:
-                pool_option = int(raw_input("Select the pool: "))
-                break
-            except IndexError:
-                print(
-                    '\tERROR: Select an existing option between'
-                    '0 and {0}.'.format(count))
-
-        print('\nConfig validation\n')
-        print('\nClient Information')
-        print(50 * '-')
-        print('Register      : {0}'.format(change))
-        print('Client Server : {0}'.format(hostname_client))
-        print('Storage Name  : {0}'.format(storage_name))
-        print('WWN Client    : {0}'.format(wwn_client))
-        print('\nStorage Information')
-        print(50 * '-')
-        print('Storage Name          : {0}'.format(get_stg.getstorage()))
-        print('Storage Type          : {0}'.format(get_stg.gettype()))
-        print('Storage SID           : {0}'.format(get_stg.getsid()))
-        print('Initiator Group Name  : {0}'.format(ign))
-        print('Making View Names     : {0}'.format(mvn))
-        print('Storage Group Name    : {0}'.format(sgn))
-        print('Storage Pool          : {0}'.format(pool_list[pool_option]))
-        print('\nInformations about the request:')
-        print(50 * '-')
-        print('Disk Volume   : {0}'.format(disk_volume))
-        print('LUN Size      : {0}'.format(lun_size))
-        print('')
+    choose_device_type()
